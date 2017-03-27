710b66d388fd057cd7ad76d07d1bc00affb97050
sylviawan <sylvia.wan97@gmail.com>
2017-03-27 01:20:17 +0100

    Register

---
+++ daredevil_register/daredevil_register/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  daredevil_register
+//
+//  Created by Sylvia Wan on 24/03/2017.
+//  Copyright © 2017 Sylvia Wan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var userName: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
