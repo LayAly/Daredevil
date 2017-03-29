1b27a407775703badde59cc6d3e8a70e77c31677
sylviawan <sylvia.wan97@gmail.com>
2017-03-29 02:04:39 +0100

    Add constraints

--- daredevil_register/daredevil_register/ViewController.swift
+++ daredevil_register/daredevil_register/ViewController.swift
@@ -6,12 +6,34 @@
 //  Copyright © 2017 Sylvia Wan. All rights reserved.
 //
 
+
+// Confirm password
+// DOB
+
+//
+
+
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var firstName: UITextField!
+    
+    @IBOutlet weak var lastName: UITextField!
+    
+    @IBOutlet weak var dateOfBirth: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
     
     @IBOutlet weak var userName: UITextField!
 
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var confirmPassword: UITextField!
+    
+    @IBAction func submitForm(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
